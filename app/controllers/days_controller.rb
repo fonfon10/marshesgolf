08e79adc59fa95bf99a3757abfe98ba7f094216b
Serge Lafontaine <slafontaine10@gmail.com>
2018-12-09 21:30:28 -0500

    fixed day/timeslots ordering

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -10,7 +10,7 @@ end
 def show
 
 	day = Day.find(params[:id])
-	@reservations = Reservation.where('day_id = ?', day.id)
+	@reservations = Reservation.where('day_id = ?', day.id).order(timeslot_id: :asc)
 end
 
 
