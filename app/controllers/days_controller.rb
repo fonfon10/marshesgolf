f6b2f62d73b8f3d0c30b68757d20bd975f251d2c
Serge Lafontaine <slafontaine10@gmail.com>
2018-12-17 22:57:53 -0500

    changes @reservations in day_controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -13,7 +13,7 @@ end
 def show
 
 	day = Day.find(params[:id])
-	@reservations = Reservation.where('day_id = ?', day.id).order(timeslot_id: :asc)
+	@reservations = Reservation.where('day_id = ?', day.id)
 #	cr = @reservations.where('member_id = ?', current_member.id)
 	if member_signed_in?
   	@count_reservations = @reservations.where('member_id = ?', current_member.id).count
