d4614b36a617f7fbd42caa5a40c993e400a512d2
Serge Lafontaine <slafontaine10@gmail.com>
2018-12-25 21:40:05 -0500

    added Carson to DB and fixed iphone6 view

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,7 +65,7 @@ open_close_type_closed = OpenClose.last
 
 
 #day
-day = Date.new(2018,12,20)
+day = Date.new(2018,12,31)
 lastday = Date.new(2019,03,31)
 
 while day <= lastday do
@@ -93,14 +93,16 @@ activity_type_closed = Activity.last
 
 
 #Member.create!(email: 'slafontaine10@gmail.com', password: 'numb10', password_confirmation: 'numb10', membership: membership_type_Admin, age_group: age_group_type_adult)
-m = Member.create!(firstname: '', lastname: '', email: 'derek.macdonald@marshesgolfclub.com', password: 'MarshesRocks', password_confirmation: 'MarshesRocks', membership: membership_type_Operator, age_group: age_group_type_adult)
+m = Member.create!(firstname: '', lastname: '', email: 'open@marshesgolfclub.com', password: 'MarshesRocks', password_confirmation: 'MarshesRocks', membership: membership_type_Operator, age_group: age_group_type_adult)
+Member.create!(firstname: 'Derek', lastname: 'MacDonald', email: 'derek.macdonald@marshesgolfclub.com', password: 'MarshesRocks', password_confirmation: 'MarshesRocks', membership: membership_type_Operator, age_group: age_group_type_adult)
+Member.create!(firstname: 'Carson', lastname: 'Scissons', email: 'carson.scissons@marshesgolfclub.com', password: 'MarshesRocks', password_confirmation: 'MarshesRocks', membership: membership_type_Operator, age_group: age_group_type_adult)
 #Member.create!(email: 'ashley@gmail.com', password: 'numb10', password_confirmation: 'numb10', membership: membership_type_Coaching, age_group: age_group_type_junior)
 #Member.create!(email: 'derek.macdonald@marshesgolfclub.com', password: 'golf123', password_confirmation: 'golf123', membership: membership_type_Coaching, age_group: age_group_type_adult)
 #Member.create!(email: 'serge@serge.com', password: 'numb10', password_confirmation: 'numb10', membership: membership_type_Operator, age_group: age_group_type_adult)
 
 
 members = [["Maddy","Pytura","michaelpytura@gmail.com","golf123",membership_type_Coaching,age_group_type_junior],
-["Stephan","du Plooy","sdpphotography1@gmail.com","golf123",membership_type_Coaching,age_group_type_junior],
+["Stephan","Du Plooy","sdpphotography1@gmail.com","golf123",membership_type_Coaching,age_group_type_junior],
 ["Ashley","Lafontaine","ashleylafontaine27@gmail.com","golf123",membership_type_Coaching,age_group_type_junior],
 ["Ian ","Clark","iclark@marks-clerk.ca","golf123",membership_type_Coaching,age_group_type_adult],
 ["Doug","Hewson","dhewson@portlandpe.com","golf123",membership_type_Coaching,age_group_type_adult],
@@ -108,7 +110,7 @@ members = [["Maddy","Pytura","michaelpytura@gmail.com","golf123",membership_type
 ["Jo-Anne","Armstrong","jo.armstrong@armelec.ca ","golf123",membership_type_Coaching,age_group_type_adult],
 ["Gillian","Burnside","gillianburnside@rogers.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Jeff","Hurdis","jeffhurdis@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
-["Rose ","de Guzman","rdeguzman@notusually.com","golf123",membership_type_Coaching,age_group_type_adult],
+["Rose ","De Guzman","rdeguzman@notusually.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Colin","Pallag","cpallag@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Peter","Strom","peter.strom6@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Kim ","Green","88kimgreen@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
@@ -119,13 +121,13 @@ members = [["Maddy","Pytura","michaelpytura@gmail.com","golf123",membership_type
 ["Dave","Beattie","dbjet@rogers.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Evan ","Smith","ensmith@rogers.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Adam ","MacRae","adam-macrae@hotmail.com","golf123",membership_type_Coaching,age_group_type_adult],
-["Ammie","Fu","fyhammie@hotmail.com ","golf123",membership_type_Coaching,age_group_type_adult],
-["Jeffrey","Zheng","abcd1@xxx.com","golf123",membership_type_Coaching,age_group_type_adult],
+["Ammie","Fu","fyhammie@hotmail.com","golf123",membership_type_Coaching,age_group_type_adult],
+["Jeffrey","Zheng","fyhammie_jeffrey@hotmail.command","golf123",membership_type_Coaching,age_group_type_adult],
 ["Serge","Lafontaine","slafontaine10@gmail.com","numb10",membership_type_Admin,age_group_type_adult],
 ["Ellis","Kinnaird","breakinnaird@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Bruce ","Hiscock","can6180@shaw.ca","golf123",membership_type_Coaching,age_group_type_adult],
 ["Kathleen","Parker","rkparker@pgonline.com","golf123",membership_type_Coaching,age_group_type_adult],
-["Jaco","du Plooy","cdplooy2@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
+["Jaco","Du Plooy","cdplooy2@gmail.com","golf123",membership_type_Coaching,age_group_type_adult],
 ["Tim ","Orr","Timothy.orr@cibc.ca","golf123",membership_type_Coaching,age_group_type_adult],
 ["Brandon","Noseworthy","brandon.noseworthy@hotmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["David","So","davidso3333@yahoo.com","golf123",membership_type_Practice,age_group_type_adult],
@@ -134,15 +136,15 @@ members = [["Maddy","Pytura","michaelpytura@gmail.com","golf123",membership_type
 ["Gregg","Shimokura","gregg.shimokura@yahoo.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Mark ","Wu","mark.mlwu@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["George ","Taronno","george.stingray@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
-["Jo ","Aldyne","joaldyne@hotmail.com","golf123",membership_type_Practice,age_group_type_adult],
-["Ken ","Hearn","Ken.Hearn@curtisswright.com ","golf123",membership_type_Practice,age_group_type_adult],
+["Jo ","Hearn","joaldyne@hotmail.com","golf123",membership_type_Practice,age_group_type_adult],
+["Ken ","Hearn","Ken.Hearn@curtisswright.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Rob ","McNeil","robert.t.mcneil@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Rocco ","Marcantonio","rocco.marcantonio@nordion.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Justin ","Saunders","quinn.cindy@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Sam ","Kloppenburg","sklopp1001@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["James","Baillie","jbaille@outlook.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Ron ","Arnold","ronharnold@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
-["Kerrey","Zheng","abcd2@xxx.com","golf123",membership_type_Practice,age_group_type_adult],
+["Kerrey","Zheng","fyhammie_kerrey@hotmail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Sean ","Miller","miller33@mail.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Norm","Wong","wong.norm@rogers.com","golf123",membership_type_Practice,age_group_type_adult],
 ["Carol","Pelger-Downing","cpelgerdowning@gmail.com","golf123",membership_type_Practice,age_group_type_adult],
